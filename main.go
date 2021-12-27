9bdf3d1b0b5e9a75fba7fc02d12e306b84dbbe94
Edwin <edwinzhou99@gmail.com>
2021-12-26 19:55:47 -0500

    add diff declarations

--- main.go
+++ main.go
@@ -8,4 +8,12 @@ func main() {
 	var i int
 	i = 42
 	fmt.Println(i)
+
+	// i := 42 will let go compiler recognize what type this is
+
+	var j int = 27
+	k := 99
+	fmt.Println(i,j,k)
+	fmt.Printf("%v, %T", j , j) //print var j and type of j
+
 }
