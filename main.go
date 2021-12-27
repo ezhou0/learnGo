7bf7b378cc63a8d8f1bd6101f4db70bc09ac6d42
Edwin <edwinzhou99@gmail.com>
2021-12-26 20:05:29 -0500

    operators

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ var (
 	companion string = "some person"
 	doctorNumber int = 3
 	season int = 11
-) //can wrap in var blocks like this for less clutter
+) //can wrap in var blocks like this for less clutter and for organization
+
+var I int = 42 //upper case, globally visable. //in main func -block scope, never visible outside block, lower case package, useable in block but not outside pkg
 
 func main() {
 	fmt.Println(" hello world")
@@ -22,7 +24,18 @@ func main() {
 	var j int = 27
 	k := 99
 	fmt.Println(i,j,k)
-	fmt.Printf("%v, %T", j , j) //print var j and type of j
-	fmt.Printf("%v, %T", l, l)
+	fmt.Printf("%v, %T\n", j , j) //print var j and type of j
+	fmt.Printf("%v, %T\n", l, l)
+
+	n := 1 == 1
+	fmt.Printf("%v, %T/n", n, n) 
+
+	a := 10
+	b := 3
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+	fmt.Println(a % b)
 
 }
